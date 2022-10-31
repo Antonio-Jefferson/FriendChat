1a7dbe3620e664d317330c622d6ffc194afe2b68
Antonio-Jefferson <antjeffersonbatista@gmail.com>
2022-10-30 23:25:35 -0300

    Projeto finalizado, mas sem todos os bônus

--- js/script.js
+++ js/script.js
@@ -1,114 +1,124 @@
-let listaDeMensagens =[];
+
+let listaDeMensagens = [];
 let userOn;
 let nome;
+
+document.addEventListener("keydown", function(evento){
+    if(evento.key === 'Enter'){
+         cadastrar();
+    }
+ })
+
 function menuMobile(){
     const menuMobile = document.querySelector('.navbar');
     menuMobile.classList.toggle('escondido');
 }
 function cadastrar(){
-    const login = document.querySelector('.login'); 
+    const login = document.querySelector('.login');
     const main = document.querySelector('.conteiner-main')
     nome = document.querySelector('.name').value;
+    if(nome === ""){
+        return;
+    }
     login.classList.remove('login');
     login.classList.add('escondido')
     main.classList.remove('escondido');
-    userName()
+    userName();
 }
 function userName(){
-    const objNome = 
+    const objNome =
     {
         name: nome
     }
     const promesi = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", objNome);
     promesi.then(respotaUser)
     function respotaUser(){
-        console.log('user add')
+        document.removeEventListener("keydown", function(evento){
+            if(evento.key === 'Enter'){
+                 cadastrar();
+            }
+         });
+        document.addEventListener("keydown", function(evento){
+            if(evento.key === 'Enter'){
+                 servSMS();
+            }
+         });
+        buscarMensagens();
+        participantesOnlines();
+        setInterval(verificarOnline, 5000);
+        setInterval(buscarMensagens, 2000);
+        setInterval(participantesOnlines, 7000);
     }
 }
-function veriricarOnline(){
-    const verificarUser = 
+
+function verificarOnline(){
+    const verificarUser =
     {
         name:nome
     }
     const verificar = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", verificarUser);
     verificar.then(ativo);
     function ativo(){
-        console.log("Presente")
-        participantesOnlines()
-    } 
-    setTimeout(veriricarOnline, 5000);
+        console.log("Presente");
+    }
 }
 
 
 
-function bucarMensagens(){
+function buscarMensagens(){
     const buscar = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
     buscar.then(resposta);
     function resposta(mensagens){
         console.log("achei");
-        listaDeMensagens = mensagens.data
-        sedMensagem() 
+        listaDeMensagens = mensagens.data;
+        listarMensagens();
     }
-    setTimeout(bucarMensagens, 3000)
 }
 
-function hora(){
-    let day = new Date();
-    let horaAtual = day.getHours();
-    let minuto = day.getMinutes()
-    let segundos = day.getSeconds()
-    time = `${horaAtual}:${minuto}:${segundos}`
-}
-document.addEventListener("keypress", function(evento){
-   if(evento.key === 'Enter'){
-        servSMS()
-        cadastrar()
-   }
-})
+
 function servSMS(){
-    let mess = document.querySelector('.text').value;
+    const mess = document.querySelector('.text').value;
     const objMess = {
         from: nome,
-        to:"nome do destinatário (Todos se não for um específico)",
+        to:"Todos",
         text: mess,
         type:"message"
     }
-    listaDeMensagens = objMess;
     const messEnviadas = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', objMess);
-    messEnviadas.then(lerResposta)
-    mess = document.querySelector('.text').value = "";
+    messEnviadas.then(lerResposta);
 }
 function lerResposta(){
     console.log("mensagem enviada");
-    sedMensagem()
+    const mess = document.querySelector('.text');
+    mess.value = "";
 }
 
-function sedMensagem(){
+function listarMensagens(){
     const ul = document.querySelector('.sms');
-    ul.innerHTML ="";
+    ul.innerHTML = "";
     for(let i = 0; i < listaDeMensagens.length; i++){
          const sms = listaDeMensagens[i].text
          const nomes = listaDeMensagens[i].from
          if(listaDeMensagens[i].type === 'message'){
             ul.innerHTML += `
             <li class="mgs">
-                <p><span>(${time})</span> <strong>${nomes}</strong>  para <strong>Todos</strong>: ${sms}</p>
+                <p><span>(${listaDeMensagens[i].time})</span> <strong>${nomes}</strong>  para <strong>Todos</strong>: ${sms}</p>
             </li>`
         }else if(listaDeMensagens[i].type === 'private_message'){
             ul.innerHTML +=
             `<li class="reservadamente">
-                 <p><span>(${time})</span> <strong>${nomes}</strong> reservadamente para <strong>Maria</strong>: ${sms}</p>
+                 <p><span>(${listaDeMensagens[i].time})</span> <strong>${nomes}</strong> reservadamente para <strong>Maria</strong>: ${sms}</p>
             </li>`
         }else{
             ul.innerHTML += `
             <li class="entrou-saiu">
-                <p><span>(${time})</span> <strong>${nomes}</strong> entra na sala...</p>
+                <p><span>(${listaDeMensagens[i].time})</span> <strong>${nomes}</strong> ${sms}</p>
             </li>`
         }
-       
+
     }
-    ul.lastElementChild.scrollIntoView()
-} 
+    ul.lastElementChild.scrollIntoView();
+}
 
 function participantesOnlines(){
     const userOnline = axios.get('https://mock-api.driven.com.br/api/v6/uol/participants');
@@ -117,14 +127,16 @@ function participantesOnlines(){
         console.log('Users encontrados')
         userOn = respostaON.data
         console.log(userOn)
-        listadeUsarios()
+        listarUsuarios();
     }
 }
-function listadeUsarios(){
+function listarUsuarios(){
     const listaOnline = document.querySelector('.listaDeOnline');
+    let online;
+    listaOnline.innerHTML = "";
     for(let i = 0; i < userOn.length; i++){
-       let online = userOn[i].name
-       listaOnline.innerHTML += ` 
+       online = userOn[i].name;
+       listaOnline.innerHTML += `
         <li onclick='selecionado(this)'>
             <div>
                 <ion-icon name="person-circle"></ion-icon>
@@ -134,8 +146,4 @@ function listadeUsarios(){
         </li>`
     }
 }
-setTimeout(listadeUsarios, 5000);
 
-hora();
-participantesOnlines()
-bucarMensagens();
